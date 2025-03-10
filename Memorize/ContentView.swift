f6662d36f4baf3e29dca9f12c18257d9b3768456
leonyannick <leon.yannik@web.de>
2025-03-10 20:58:36 +0100

    initial commit

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,17 +7,66 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    let HalloweenTheme = ["🎃", "🧙‍♀️", "👻", "🧹"]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Memorize").font(.title)
+            cards
+            themeChooser
+            
         }
         .padding()
     }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(HalloweenTheme, id: \.self) { theme in
+                CardView(content: theme)
+            }
+        }
+    }
+    
+    var themeChooser:  some View {
+        Button(action: {
+            print("hello")
+        }, label: {
+            Image(systemName: "1.lane")
+        })
+    }
 }
+
+
+
+struct CardView: View {
+    let content: String
+    
+    @State var isFlipped: Bool = false
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(.white)
+                .stroke(.purple, lineWidth: 3)
+            Text(content).font(.largeTitle)
+            Circle()
+                .fill(.purple)
+                .opacity(isFlipped ? 1 : 0)
+        }
+        .onTapGesture {
+            isFlipped.toggle()
+        }
+    }
+}
+
+
+
+
+
+
 
 #Preview {
     ContentView()
