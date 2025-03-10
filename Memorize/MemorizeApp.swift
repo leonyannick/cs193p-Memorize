e1bc7e36ec6e56f8260a619700aed72669bc8f19
leonyannick <leon.yannik@web.de>
2025-03-10 14:14:36 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Leon Baumann on 10.03.25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
